e1a22080713956231f6bd28683e8efb394797aec
Devin Gelineau <keelog7331@gmail.com>
2022-08-07 16:51:20 -0400

    fix(blog): fix anchor tags
    add scrollPaddingTop to html for anchor tag navigation

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,18 @@
+import { CSSProperties } from "react";
+
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
 import { createGetInitialProps } from "@mantine/next";
 
+import settings from "@/config/settings";
+
 const getInitialProps = createGetInitialProps();
+
+/* Setting the scrollPaddingTop to the value of the headerSize so that anchor
+ navigation takes the height of the header into account with navigating */
+const htmlStyle: CSSProperties = {
+  scrollPaddingTop: settings.headerSize,
+};
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export default class _Document extends Document {
@@ -11,7 +21,7 @@ export default class _Document extends Document {
   // eslint-disable-next-line class-methods-use-this
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" style={htmlStyle}>
         <Head />
         <body>
           <Main />
